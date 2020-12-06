4f3e2eae6fd4e9215a73a882184dc1c936c79ae8
Mackan <52716811+VindProGaming@users.noreply.github.com>
2020-12-06 18:42:36 +0100

    first commit

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ var welcomeText = document.getElementById("welcometext");
 console.log(welcomeText);
 
 function StartTraining() {}
+
+function GenareteNew() {
+  var Amount = document.getElementById("wordnumberamount").value;
+  console.log(Amount);
+}
