314da5bf8894602ac96966301cf5eca64464245f
Mackan Dev <vpgstudiodev@gmail.com>
2020-12-08 17:35:54 +0000

    Workt a lot on repl it and added a new js file for handeling

--- main/index.js
+++ main/index.js
@@ -1,3 +1,4 @@
+
 console.log("Opening sucses");
 
 var welcomeText = document.getElementById("welcometext");
---
+++ main/wordTesting.js
@@ -0,0 +1,10 @@
+function test(){
+  var fr1 = document.getElementById("fr1").value;
+  var sv1 = document.getElementById("sv1").value;
+  var fr2 = document.getElementById("fr1").value;
+  var sv2 = document.getElementById("sv1").value;
+
+
+  localStorage.setItem("fr1", fr1);
+  localStorage.setItem("sv1", sv1);
+}
